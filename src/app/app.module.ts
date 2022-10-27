0a3dc2aee02324da0ff9f75f4fa2e821fa9acc88
Mcebo Xaba <mcebo@paycurve.co.za>
2022-10-27 11:34:46 +0200

    Home and Nav bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,23 @@
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
+    NavComponent
   ],
   imports: [
     NgbModule,
     BrowserModule,
     AppRoutingModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent],
-  schemas: [
-    CUSTOM_ELEMENTS_SCHEMA
-  ],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
